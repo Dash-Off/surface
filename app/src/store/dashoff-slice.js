5a96ea066de64ee9bcc7ec3255c1544957a6c5ec
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-07-20 08:48:28 -0400

    Implement current dashoff store

---
+++ app/src/store/dashoff-slice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initiateState = {
+  current: {},
+};
+
+const formatCurrentDashOff = (dashOff) => {
+  if (dashOff.type != "Owner") return {};
+  return {
+    id: dashOff.dashOff._id,
+    type: dashOff.dashOff.type,
+    duration: dashOff.challenge.duration,
+    title: dashOff.challenge.name,
+    headline: dashOff.challenge.headline,
+    description: dashOff.challenge.description,
+    createdAt: dashOff.dashOff.createdAt,
+  };
+};
+
+export const dashOffSlice = createSlice({
+  name: "dashOff",
+  initialState: initiateState,
+  reducers: {
+    loadCurrentDashOff: (state, action) => {
+      return {
+        ...state,
+        current: formatCurrentDashOff(action.payload),
+      };
+    },
+  },
+});
+
+export const { loadCurrentDashOff } = dashOffSlice.actions;
+
+export const getCurrent = () => {
+  return (state) => state.dashOff.current;
+};
+
+export default dashOffSlice.reducer;
--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersSlice } from "./user-slice";
+import { dashOffSlice } from "./dashoff-slice";
 export const store = configureStore({
   reducer: {
     [usersSlice.name]: usersSlice.reducer,
+    [dashOffSlice.name]: dashOffSlice.reducer,
   },
 });
