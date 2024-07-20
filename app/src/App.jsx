a98d853b4ed158c83897a10c96f334a714ad1b0b
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-07-20 08:51:19 -0400

    Add editor page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,31 +5,31 @@ import theme from "./theme.js";
 //import Test from './pages/Test/Test';
 import { CssBaseline } from "@mui/material";
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Test from './pages/Test/Test';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Test from "./pages/Test/Test";
 import Auth from "./pages/Auth/Auth.jsx";
 import Dashboard from "./pages/Dashboard/Dashboard.jsx";
 import Dashboard1 from "./pages/Dashboard/Dashboard1.jsx";
 import DashboardSlider from "./components/DashboardSlider/DashboardSlider.jsx";
 import Sidebar1 from "./components/Sidebar/Sidebar1.jsx";
+import Editor from "./pages/Editor/index.jsx";
 
 const App = () => {
-
   const routes = (
     <Router>
       <Routes>
-      <Route path="/" exact element={<Test/>}/>
-        <Route path="/home" exact element={<Home/>}/>
-        <Route path="/auth" exact element={<Auth/>}/>
-        <Route path="/dashboard" exact element={<Dashboard/>}/>
-        <Route path="/dashboard1" exact element={<Dashboard1/>}/>
-        <Route path="/slider" exact element={<DashboardSlider/>}/>
-        <Route path="/side" exact element={<Sidebar1/>}/>
+        <Route path="/" exact element={<Test />} />
+        <Route path="/home" exact element={<Home />} />
+        <Route path="/auth" exact element={<Auth />} />
+        <Route path="/dashboard" exact element={<Dashboard />} />
+        <Route path="/dashboard1" exact element={<Dashboard1 />} />
+        <Route path="/slider" exact element={<DashboardSlider />} />
+        <Route path="/side" exact element={<Sidebar1 />} />
+        <Route path="/space/:id" exact element={<Editor />} />
       </Routes>
     </Router>
   );
-
 
   return (
     <>
